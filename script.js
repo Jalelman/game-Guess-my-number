f0a0a083f05a95aeb8452a793b28dcb0cc896f23
Jalelman <64267966+Jalelman@users.noreply.github.com>
2023-03-15 18:38:45 +0100

    half code without again button

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 'use strict';
+let secretNumber=Math.trunc(Math.random()*20)+1;
+let score=20;
+let hightscore=0;
+console.log(secretNumber)
+const displayMessage = function(message){
+    document.querySelector(".message").textContent=message;
+};
+document.querySelector(".check").addEventListener("click",function(){
+    const guess=Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess)
+    if(!guess){
+        displayMessage('theree is no number');
+
+    }else if(guess===secretNumber){
+        displayMessage('Correct Number');
+        document.querySelector(".number").textContent=secretNumber;
+        document.querySelector("body").style.backgroundColor='#60b347';
+        document.querySelector('.number').style.width='30rem';
+        if(score>hightscore){
+            document.querySelector(".highscore").textContent=score;
+        }
+
+    }else if(guess !==secretNumber){
+        if(score>1){
+            displayMessage(guess>secretNumber? "tooHight":"too Low");
+            score--;
+            document.querySelector(".score").textContent=score;
+
+        }else{
+            displayMessage('game over');
+            document.querySelector(".score").textContent=0;
+        }
+    }
+
+});
